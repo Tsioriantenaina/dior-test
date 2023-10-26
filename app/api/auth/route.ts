82dcc902f0fe11040ac453733aac4d9d22698db1
Tsioriantenaina <tianatsimindrana@yahoo.com>
2023-10-27 00:13:14 +0200

    Remove console

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
 	const body = await request.json();
-	console.log(body);
+
 	const { login, password } = body.data;
 
 	if (login !== "admin" || password !== "admin") {
